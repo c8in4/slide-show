10d1befb74c25a8e5113f815de6a1663e4c118a5
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-09-13 21:22:55 +1200

    First functional version of a basic carousel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+import dropDownMenu from "@c8in4/drop-down-npm"
 import "./style.css"
 
 console.log("hello from JS")
+
+dropDownMenu('menu')
+
+const slidesContainer = document.querySelector('.slides-container')
+const previousImageButton = document.querySelector('.previous-image')
+const nextImageButton = document.querySelector('.next-image')
+
+let slidePosition = 0
+
+previousImageButton.addEventListener('click', () => {
+  console.log('previous image')
+  slidePosition -= 100
+  if (slidePosition < 0) slidePosition = 400
+  slidesContainer.style.left = `-${slidePosition}%`
+})
+
+nextImageButton.addEventListener('click', () => {
+  console.log('previous image')
+  slidePosition += 100
+  if (slidePosition > 400) slidePosition = 0
+  slidesContainer.style.left = `-${slidePosition}%`
+})
