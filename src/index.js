068e2417b6de2aaee05c17a1d62de1c49cb229a4
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-09-14 10:07:23 +1200

    Add circles under slide-show

--- src/index.js
+++ src/index.js
@@ -1,26 +1,70 @@
-import dropDownMenu from "@c8in4/drop-down-npm"
 import "./style.css"
+import dropDownMenu from "@c8in4/drop-down-npm"
+import Img1 from "./images/01.jpg"
+import Img2 from "./images/02.jpg"
+import Img3 from "./images/03.jpg"
+import Img4 from "./images/04.jpg"
+import Img5 from "./images/05.jpg"
 
-console.log("hello from JS")
+// console.log("hello from JS")
 
-dropDownMenu('menu')
+dropDownMenu("menu")
 
-const slidesContainer = document.querySelector('.slides-container')
-const previousImageButton = document.querySelector('.previous-image')
-const nextImageButton = document.querySelector('.next-image')
+const slidesContainer = document.querySelector(".slides-container")
+const slideCircles = document.querySelector(".circles-container")
 
-let slidePosition = 0
+const images = [Img1, Img2, Img3, Img4, Img5]
 
-previousImageButton.addEventListener('click', () => {
-  console.log('previous image')
-  slidePosition -= 100
-  if (slidePosition < 0) slidePosition = 400
-  slidesContainer.style.left = `-${slidePosition}%`
+let currentSlidePosition = 0
+const lastSlidePosition = images.length - 1
+
+renderCircles()
+
+images.forEach((image, index) => {
+  const imgElement = document.createElement("img")
+  imgElement.src = image
+  imgElement.alt = `Step ${index + 1}`
+  slidesContainer.append(imgElement)
 })
 
-nextImageButton.addEventListener('click', () => {
-  console.log('previous image')
-  slidePosition += 100
-  if (slidePosition > 400) slidePosition = 0
-  slidesContainer.style.left = `-${slidePosition}%`
-})
+const previousImageButton = document.querySelector(".previous-image")
+previousImageButton.addEventListener("click", previousImage)
+
+const nextImageButton = document.querySelector(".next-image")
+nextImageButton.addEventListener("click", nextImage)
+
+function nextImage() {
+  currentSlidePosition++
+  if (currentSlidePosition > lastSlidePosition) currentSlidePosition = 0
+  moveToImage()
+}
+
+function previousImage() {
+  currentSlidePosition--
+  if (currentSlidePosition < 0) currentSlidePosition = lastSlidePosition
+  moveToImage()
+}
+
+function moveToImage() {
+  slidesContainer.style.left = `-${currentSlidePosition}00%`
+  renderCircles()
+}
+
+function renderCircles() {
+  slideCircles.innerHTML = ""
+  for (let i = 0; i < images.length; i++) {
+    const circle = document.createElement("button")
+    if (i == currentSlidePosition) {
+      circle.classList.add("active")
+    } else {
+      circle.classList.remove("active")
+    }
+    slideCircles.append(circle)
+  }
+  slideCircles.childNodes.forEach((circle, index) => {
+    circle.addEventListener("click", () => {
+      currentSlidePosition = index
+      moveToImage()
+    })
+  })
+}
